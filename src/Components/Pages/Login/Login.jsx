eddd144fc45e4a4ead53ce66f8cfcff89d0e8353
Azir@9200 <aziruddin83@hotmail.com>
2023-10-24 17:34:11 +0100

    test commit

--- src/Components/Pages/Login/Login.jsx
+++ src/Components/Pages/Login/Login.jsx
@@ -1,66 +1,68 @@
 import { useContext } from "react";
 import { AuthContext } from "../../../Providers/Provider";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+
 
 const Login = () => {
-
-  const { userLogin } = useContext(AuthContext)
-  const handleLogin = (event) => {
-    event.preventDefault();
-    const form = event.target;
-    const email = form.email.value;
-    const password = form.password.value;
-    console.log(email, password);
-    userLogin(email, password);
-    .then(result => {
-      console.log(result.user)
-    })
-
-  }
-// const handleSignOut = () => {
-//   signOut()
-//     .then(result => {
-//       console.log(result);
-//     })
-//     .catch(error => {
-//       console.log(error)
-//     })
-// }
+  const { userLogin } = useContext(AuthContext);
+  const navigate = useNavigate()
 
 
-return (
-  <div className="hero min-h-screen bg-base-200">
-    <div className="hero-content ">
-      <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const form = new FormData(e.currentTarget);
+    const userEmail = form.get("email");
+    const userPassword = form.get("password");
 
-        <h1 className="text-5xl font-bold">Login now!</h1>
-        <form onSubmit={handleLogin} className="card-body">
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">Email</span>
-            </label>
-            <input type="email" name="email" placeholder="email" className="input input-bordered" required />
-          </div>
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">Password</span>
-            </label>
-            <input type="password" name="password" placeholder="password" className="input input-bordered" required />
-            <label className="label">
-              <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-            </label>
-          </div>
-          <div className="form-control mt-6">
-            <button className="btn btn-primary">Login</button>
-            <label className="label">
-              <a href="#" className="label-text-alt link link-hover">Already Have an Account ?  <Link to='/register' >Register</Link> </a>
-            </label>
-          </div>
-        </form>
+
+    userLogin(userEmail, userPassword)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log(user);
+        navigate(location?.state ? location.state : "/");
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+      });
+  };
+
+
+
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content ">
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+
+          <h1 className="text-5xl font-bold">Login now!</h1>
+          <form onSubmit={handleLogin} className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input type="email" name="email" placeholder="email" className="input input-bordered" required />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input type="password" name="password" placeholder="password" className="input input-bordered" required />
+              <label className="label">
+                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-primary">Login</button>
+              <label className="label">
+                <a href="#" className="label-text-alt link link-hover">Already Have an Account ?  <Link to='/register' >Register</Link> </a>
+              </label>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
 };
 
 export default Login;
